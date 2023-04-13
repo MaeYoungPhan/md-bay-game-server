4add699b053f040a4c0be345adc8b1066f4ccc15
Maegan Phan <maegyoung@gmail.com>
2023-04-13 16:09:28 -0500

    changed rivers and streams model to include longitude and latitude. Started to update fixtures

--- baygameapi/models/river_and_stream.py
+++ baygameapi/models/river_and_stream.py
@@ -3,3 +3,6 @@ from django.db import models
 class RiverAndStream(models.Model):
     name = models.CharField(max_length = 50)
     miles_to_bay = models.DecimalField(max_digits=5, decimal_places=2)
+    latitude = models.DecimalField(max_digits=2, decimal_places=7)
+    longitude = models.DecimalField(max_digits=2, decimal_places=7)
+
